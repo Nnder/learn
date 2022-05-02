d5002ccbf58f5c9379aa73d0ef2046c469a6d3fb
Nnder <asfahaikrkt6k56@yandex.ru>
2022-05-02 22:46:27 +0500

    Полиморфизм

--- OOP/index.ts
+++ OOP/index.ts
@@ -126,3 +126,99 @@ db.createNewTable(123)
 db.tables
 
 
+
+class Person {
+    private _firstName;
+    private _lastName;
+    private _age;
+
+    constructor(firstName, lastName, age) {
+        this._age = age;
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+
+    public greeting(){
+        console.log(`Привет я человек ${this._firstName}`);
+    }
+
+    public get fullName(){
+        return `имя ${this._firstName} фамилия ${this._lastName}`;
+    }
+
+    get firstName(){
+        return this._firstName
+    }
+
+    set firstName(name){
+        this._firstName = name
+    }
+
+    get lastName(){
+        return this._lastName
+    }
+
+    set lastName(name){
+        this._lastName = name
+    }
+
+    get age(){
+        return this._age
+    }
+
+    set age(age){
+        if(age < 0){
+            this._age = 0;
+        } else {
+            this._age = age
+        }
+    }
+}
+
+class Employee extends Person {
+
+    private inn;
+    private number;
+    private snils;
+
+    constructor(firstName, lastName, age, inn, number, snils) {
+        super(firstName,lastName,age);
+        this.inn = inn;
+        this.number = number;
+        this.snils = snils;
+    }
+
+    public greeting(){
+        console.log(`Привет я работник ${this.firstName}`);
+    }
+}
+
+
+class Developer extends Employee {
+    private level;
+
+    constructor(firstName, lastName, age, inn, number, snils, level) {
+        super(firstName,lastName,age,inn,number,snils);
+        this.level = level
+    }
+
+    public greeting(){
+        console.log(`Привет я программист ${this.firstName}`);
+    }
+}
+
+const person1 = new Person('name', 'lastname', '30');
+const Employee1 = new Employee('name', 'lastname', '30', 23123, 4412424214, 521555);
+const programmer = new Developer('name', 'lastname', '30', 23123, 4412424214, 521555, 'Junior');
+
+
+const PersonList: Person[] = [person1, Employee1, programmer];
+
+function massGreeting(persons: Person[]){
+    for(let i = 0; i< persons.length; i++){
+        const person = persons[i];
+        person.greeting();
+    } 
+}
+
+massGreeting(PersonList)
